343e844f4e35cebfb70ae5b958a4d041fca8dfe8
0x0961h <0x0961h@gmail.com>
2015-12-10 10:26:48 +0300

    Day 09 Solution

--- src/main/java/x0961h/adventofcode/day09/DaySolver.java
+++ src/main/java/x0961h/adventofcode/day09/DaySolver.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.IntStream;
 
 /**
  * Created by 0x0961h on 09.12.2015.
@@ -12,8 +11,7 @@ import java.util.stream.IntStream;
 public class DaySolver {
     public static void main(String[] args) throws IOException {
         String input = new String(Files.readAllBytes(Paths.get("src/main/resources", "day09.data")));
-        solve(input);
-//        System.out.println("Result = " + solve(input));
+        System.out.println("Result = " + solve(input));
     }
 
     public static int solve(String input) {
@@ -38,8 +36,6 @@ public class DaySolver {
                     temp.put(new AbstractMap.SimpleEntry<>(from, to), routeLength);
                     if (!cities.containsKey(from)) cities.put(from, cities.size());
                     if (!cities.containsKey(to)) cities.put(to, cities.size());
-
-                    System.out.println(line);
                 });
 
         names = new String[cities.size()];
@@ -53,115 +49,35 @@ public class DaySolver {
             distances[cities.get(entry.getKey().getValue())][cities.get(entry.getKey().getKey())] = entry.getValue();
         }
 
-        System.out.println();
-        System.out.println(Arrays.deepToString(distances).replaceAll("\\], \\[", "]\n[").replaceAll("\\]\\]", "]").replaceAll("\\[\\[", "["));
-        System.out.println();
+        return iteration(cities.size(), distances, new ArrayList<>());
+    }
 
-        for (int city1 = 0; city1 < cities.size(); city1++) {
-            List<Integer> alreadyVisited1 = new ArrayList<>();
-            alreadyVisited1.add(city1);
+    private static int iteration(int citiesCount, int[][] distances, List<Integer> levels) {
+        int result = Integer.MAX_VALUE;
 
-            for (int city2 = 0; city2 < cities.size(); city2++) {
-                if (alreadyVisited1.contains(city2)) continue;
-                List<Integer> alreadyVisited2 = new ArrayList<>(alreadyVisited1);
-                alreadyVisited2.add(city2);
+        for (int i = 0; i < citiesCount; i++) {
+            if (!levels.isEmpty() && levels.contains(i)) continue;
+            levels.add(i);
 
-                for (int city3 = 0; city3 < cities.size(); city3++) {
-                    if (alreadyVisited2.contains(city3)) continue;
-                    List<Integer> alreadyVisited3 = new ArrayList<>(alreadyVisited2);
-                    alreadyVisited3.add(city3);
-
-                    for (int city4 = 0; city4 < cities.size(); city4++) {
-                        if (alreadyVisited3.contains(city4)) continue;
-                        List<Integer> alreadyVisited4 = new ArrayList<>(alreadyVisited3);
-                        alreadyVisited4.add(city4);
-
-                        for (int city5 = 0; city5 < cities.size(); city5++) {
-                            if (alreadyVisited4.contains(city5)) continue;
-                            List<Integer> alreadyVisited5 = new ArrayList<>(alreadyVisited4);
-                            alreadyVisited5.add(city5);
-
-                            for (int city6 = 0; city6 < cities.size(); city6++) {
-                                if (alreadyVisited5.contains(city6)) continue;
-                                List<Integer> alreadyVisited6 = new ArrayList<>(alreadyVisited5);
-                                alreadyVisited6.add(city6);
-
-                                for (int city7 = 0; city7 < cities.size(); city7++) {
-                                    if (alreadyVisited6.contains(city7)) continue;
-                                    List<Integer> alreadyVisited7 = new ArrayList<>(alreadyVisited6);
-                                    alreadyVisited7.add(city7);
-
-                                    for (int city8 = 0; city8 < cities.size(); city8++) {
-                                        if (alreadyVisited7.contains(city8)) continue;
-                                        List<Integer> alreadyVisited8 = new ArrayList<>(alreadyVisited7);
-                                        alreadyVisited8.add(city8);
-
-                                        System.out.println(alreadyVisited8);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            if (levels.size() == citiesCount) {
+                result = Math.min(result, calcPathLength(levels, distances));
+            } else {
+                result = Math.min(result, iteration(citiesCount, distances, levels));
             }
+
+            levels.remove(levels.size() - 1);
         }
 
-//        for (int city1 = 0; city1 < cities.size(); city1++) {
-//            System.out.println("Start from city " + names[city1]);
-//            for (int city2 = 0; city2 < cities.size(); city2++) {
-//
-//            }
-//            System.out.println();
-//        }
-
-        return 0;
+        return result;
     }
 
-    private static int pickCity(int citiesCount, List<Integer> visited) {
-        int index = 0;
+    private static int calcPathLength(List<Integer> levels, int[][] distances) {
+        int length = 0;
 
-        while (visited.contains(index) && index < citiesCount) index++;
-
-        if (index == citiesCount)
-            return -1;
-        else
-            return index;
-    }
-
-    private class Node {
-
-    }
-
-    private static final int INF = Integer.MAX_VALUE;
-
-    public static void solve(String[] cities, int[][] distances) {
-        int[] dist = IntStream.range(0, cities.length).map(r -> r == 0 ? 0 : INF).toArray();
-        Set<Integer> sptSet = new HashSet<>();
-
-        while (sptSet.size() < cities.length) {
-            iteration(cities, distances, dist, sptSet);
-        }
-        System.out.println(Arrays.toString(dist));
-        System.out.println("[0, 4, 12, 19, 21, 11, 9, 8, 14]");
-    }
-
-    private static void iteration(String[] cities, int[][] distances, int[] dist, Set<Integer> sptSet) {
-        int min = INF;
-        int minIndex = -1;
-
-        for (int i = 0; i < dist.length; i++) {
-            if (dist[i] <= min && !sptSet.contains(i)) {
-                min = dist[i];
-                minIndex = i;
-            }
+        for (int i = 0; i < levels.size() - 1; i++) {
+            length += distances[levels.get(i)][levels.get(i + 1)];
         }
 
-        sptSet.add(minIndex);
-        for (int i = 0; i < cities.length; i++) {
-            if (minIndex == i) continue;
-            if (distances[minIndex][i] != -1 && !sptSet.contains(i) && dist[i] >= dist[minIndex] + distances[minIndex][i]) {
-                dist[i] = dist[minIndex] + distances[minIndex][i];
-            }
-        }
+        return length;
     }
 }
--- src/test/java/x0961h/adventofcode/day09/DaySolverTest.java
+++ src/test/java/x0961h/adventofcode/day09/DaySolverTest.java
@@ -1,5 +1,6 @@
 package x0961h.adventofcode.day09;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 /**
@@ -8,57 +9,8 @@ import org.junit.Test;
 public class DaySolverTest {
     @Test
     public void test() {
-        String[] cities = { "London", "Dublin", "Belfast" };
-        int[][] distances = new int[cities.length][cities.length];
-        distances[0][1] = 464;
-        distances[1][0] = 464;
-        distances[0][2] = 518;
-        distances[2][0] = 518;
-        distances[1][2] = 141;
-        distances[2][1] = 141;
-
-        DaySolver.solve(cities, distances);
-    }
-
-    @Test
-    public void graphTest() {
-        String[] cities = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
-        int[][] distances = new int[cities.length][cities.length];
-
-        for (int i = 0; i < cities.length; i++) {
-            for (int j = 0; j < cities.length; j++) {
-                distances[i][j] = -1;
-            }
-        }
-
-        distances[0][1] = 4;
-        distances[0][7] = 8;
-        distances[1][7] = 11;
-        distances[1][2] = 8;
-        distances[2][8] = 2;
-        distances[2][5] = 4;
-        distances[2][3] = 7;
-        distances[3][4] = 9;
-        distances[3][5] = 14;
-        distances[4][5] = 10;
-        distances[5][6] = 2;
-        distances[6][7] = 1;
-        distances[7][8] = 7;
-
-        distances[1][0] = 4;
-        distances[7][0] = 8;
-        distances[7][1] = 11;
-        distances[2][1] = 8;
-        distances[8][2] = 2;
-        distances[5][2] = 4;
-        distances[3][2] = 7;
-        distances[4][3] = 9;
-        distances[5][3] = 14;
-        distances[5][4] = 10;
-        distances[6][5] = 2;
-        distances[7][6] = 1;
-        distances[8][7] = 7;
-
-        DaySolver.solve(cities, distances);
+        Assert.assertEquals(605, DaySolver.solve("London to Dublin = 464\n" +
+                "London to Belfast = 518\n" +
+                "Dublin to Belfast = 141"));
     }
 }
